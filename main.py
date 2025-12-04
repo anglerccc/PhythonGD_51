6264ac2758dd226870fcc0092a4c92f0038e803c
Урж Александра <aleksa.urzhumova@mail.ru>
2025-12-04 21:08:16 +0300

    [ужас]

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
+numbers = str(input("Введите число знак число: "))
+first = numbers[0]
+second = numbers[2]
 
-
-
-
-
-
+for i in numbers:
+    if i == "+":
+        print()
+    else:
+        continue
 
 
 
